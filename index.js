29194fa933724605af48827159306abe076f533f
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-10-01 13:29:39 +0300

    Tests are Done!

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(str = "roller-skate"){
+    return `This Saturday, I want to ${str}!`;
+}
+
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(adj){
+    const inner = function (val = "special"){
+        return `You are ${adj}${val}${adj}!`
+    }
+    return inner;
+}
